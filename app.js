bb6dd9e732472fdbd539f766a1de15e5984740b3
Benoit Gacher <benoitgacher@gmail.com>
2023-08-15 12:09:54 +0200

    server ok

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const express = require('express')  // fetching express dependency with require keyword from node module folder
+
+const app = express(); // creating an instance of an express application
+const port = 3000 // Define a port 
+
+app.get('/',(req,res) => res.send('Hello,Express, we come back ! ')); // Define endpoint with get method
+// First parameter , defautlt URL
+// Function with two argument : 
+//
+//  -req : request object corresponding to the request recieve  by the endpoint
+//  -res : response , object to send to the client
+
+app.listen(port,() => console.log(`Notre app Node esrt démarré sur : http://localhost:${port}`)); // Start API on port with listen method given by express
