9340071aa6b6ace0f4e842f47e81ecada052c518
Benoit Gacher <benoitgacher@gmail.com>
2023-08-23 15:17:05 +0200

    add findAndCountAll method

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -3,24 +3,28 @@ const { Pokemon } = require('../db/sequelize');
 // Import sequelize operator
 const { Op } = require('sequelize');
 
-module.exports = (app) => {                                                // Export function who take in parameter all apllication.With this we can separate route simpliest and in different modules
-    app.get('/api/pokemons',(req,res) =>{                                  // We use findAll method who return a promise with all pokemons in DB
-      if(req.query.name) {                                                 // We extract name parameter with req.query and use if to separate two cases
-        console.log(req.query.name)                                         
+
+module.exports = (app) => {                                    // Export function who take in parameter all apllication.With this we can separate route simpliest and in different modules
+    app.get('/api/pokemons',(req,res) =>{                      // We use findAndCount method who return a promise with all pokemons in DB
+      if(req.query.name) {                                     // We extract name parameter with req.query and use if to separate two cases                                        
         const name = req.query.name;
-        return Pokemon.findAll({
-           where: {                                                         // We use sequilize operator to return a list instead a strict equality if we used previous method( where: {name:name})
-             name: {                                                        // name is pokemon's model property
-        [Op.like]: `%${name}%`                                              // name is our search's criterion
+        return Pokemon.findAndCountAll({
+           where: {                                            // We use sequilize operator to return a list instead a strict equality if we used previous method( where: {name:name})
+             name: {                                           // name is pokemon's model property
+        [Op.like]: `%${name}%`                                 // name is our search's criterion
              }
-            } 
+            },
+            limit: 5,                                          // We add limit operator to send a maximum of 5 results
+            order: [['name', 'ASC']]                           // We add order by ASC (Alphabetics)                         
           })                    
-        .then(pokemons => {
-          const message = `Il y a ${pokemons.length} pokémons qui correspondent au terme de recherche ${name}.`
-          res.json({ message, data: pokemons })
+        .then(({count, rows })=> {                                              // We get the two variable returned by findAndCountAll (count for number of every group and rows for data)
+          const message = `Il y a ${count} pokémons qui correspondent au terme de recherche ${name}.`
+          res.json({ message, data: rows })
         })
       }else {
-        Pokemon.findAll()                                                  
+        Pokemon.findAll(
+          {order: [['name', 'ASC']]}
+        )                                                  
           .then(pokemons => {                                              
             const message = 'La liste des pokémons à bien été récupéré.'   
             res.json({message, data: pokemons})                            
