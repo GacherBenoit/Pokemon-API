4c31b6327cf3b76aabc8f9fa417d47d7c2cf4149
Benoit Gacher <benoitgacher@gmail.com>
2023-08-23 16:32:07 +0200

    findAll checking added

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -8,22 +8,28 @@ module.exports = (app) => {                                    // Export functio
     app.get('/api/pokemons',(req,res) =>{                      // We use findAndCount method who return a promise with all pokemons in DB
       if(req.query.name) {                                     // We extract name parameter with req.query and use if to separate two cases                                        
         const name = req.query.name;
+        const limit = parseInt(req.query.limit)|| 5            // Need to parse for not send a string for SQL resquest and define at 5 if we don't have parameters by client
+        
+        if(name.length < 2) {                                  // We check if client send a search with a string'length of 2
+          const message = 'Le terme de la recherche doit contenir au moins 2 caractère.'
+          return res.status(400).json({message})
+        }
         return Pokemon.findAndCountAll({
            where: {                                            // We use sequilize operator to return a list instead a strict equality if we used previous method( where: {name:name})
              name: {                                           // name is pokemon's model property
         [Op.like]: `%${name}%`                                 // name is our search's criterion
              }
             },
-            limit: 5,                                          // We add limit operator to send a maximum of 5 results
-            order: [['name', 'ASC']]                           // We add order by ASC (Alphabetics)                         
+           order: [['name', 'ASC']],                       // We add order by ASC (Alphabetics)
+            limit: limit,                                  // We add limit operator to send by req.query.limit                                                 
           })                    
-        .then(({count, rows })=> {                                              // We get the two variable returned by findAndCountAll (count for number of every group and rows for data)
-          const message = `Il y a ${count} pokémons qui correspondent au terme de recherche ${name}.`
+      .then(({count, rows })=> {                           // We get the two variable returned by findAndCountAll (count for number of every group and rows for data)
+          const message = `Il y a ${count} pokémons qui correspondent au terme de recherche: ${name}.`
           res.json({ message, data: rows })
         })
       }else {
         Pokemon.findAll(
-          {order: [['name', 'ASC']]}
+          {order: [['name', 'ASC']]},
         )                                                  
           .then(pokemons => {                                              
             const message = 'La liste des pokémons à bien été récupéré.'   
@@ -33,6 +39,6 @@ module.exports = (app) => {                                    // Export functio
             const message = `La liste des pokémons n'as pas pu être récupérée. Réessayez dans quelques instants`
             res.status(500).json({message, data: error}) 
           })
-      }                                 
+      }                          
     });
 }
